04145159bf5f8e0f549b709093224f7517337249
Raag Naidu <raag.naidu@gmail.com>
2020-12-31 06:43:51 +0530

    Fix battery drain due to Timer
    - Fetch data onCreate
    - Implemented SwipeToRefresh

--- app/src/main/java/com/example/cryptowatcher/MainActivity.java
+++ app/src/main/java/com/example/cryptowatcher/MainActivity.java
@@ -9,50 +9,46 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class MainActivity extends WearableActivity implements View.OnClickListener {
+public class MainActivity extends WearableActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     TextView txt;
     Timer timer;
-    AsyncTask<String, String, JSONObject> runningTask;
+    AsyncTask<Void, Void, ArrayList<JSONObject>> runningTask;
+
+    private final String API_URL = "https://api.coingecko.com/api/v3/coins/";
+    //private final String[] mCoinList = getResources().getStringArray(R.array.coins_supported);
+    private String[] mtickerList = new ArrayList<String>().toArray(new String[0]);
+    private SwipeRefreshLayout pullToRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt = findViewById(R.id.textViewCel);
+        mtickerList = getResources().getStringArray(R.array.api_ticker);
 
-        // Because we implement OnClickListener, we only
-        // have to pass "this" (much easier)
-        txt.setOnClickListener(this);
-        startTimer();
+        runningTask = new LongOperation();
+        runningTask.execute();
+
+        pullToRefresh = findViewById(R.id.swiperefresh);
+        pullToRefresh.setOnRefreshListener(this);
     }
-
-    @Override
-    public void onClick(View view) {
-        // Detect the view that was "clicked"
-        switch (view.getId()) {
-            case R.id.textViewCel:
-                if (runningTask != null)
-                    runningTask.cancel(true);
-                runningTask = new LongOperation();
-                runningTask.execute("https://api.coingecko.com/api/v3/coins/celsius-degree-token");
-                break;
-        }
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -61,63 +57,65 @@ public class MainActivity extends WearableActivity implements View.OnClickListen
             runningTask.cancel(true);
     }
 
-    private void startTimer() {
-        timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                new LongOperation().execute("https://api.coingecko.com/api/v3/coins/celsius-degree-token");
-                new LongOperation().execute("https://api.coingecko.com/api/v3/coins/bitcoin");
-                new LongOperation().execute("https://api.coingecko.com/api/v3/coins/ethereum");
-            }
-        }, 0, 10);
+    @Override
+    public void onRefresh() {
+        if (runningTask != null)
+            runningTask.cancel(true);
+        runningTask = new LongOperation();
+        runningTask.execute();
     }
 
-    private final class LongOperation extends AsyncTask<String, String, JSONObject> {
-
+    private final class LongOperation extends AsyncTask<Void, Void, ArrayList<JSONObject>> {
         @Override
-        protected JSONObject doInBackground(String... urls) {
-            URL url = null;
-            try {
-                url = new URL(urls[0]);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
+        protected ArrayList<JSONObject> doInBackground(Void... voids) {
+            ArrayList<JSONObject> result = new ArrayList<JSONObject>();
             JSONObject json_var = null;
-            try {
-                json_var = new JSONObject(IOUtils.toString(url, StandardCharsets.UTF_8));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+            URL url = null;
+            for (String coin : mtickerList){
+                try {
+                    url = new URL(API_URL + coin);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    json_var = new JSONObject(IOUtils.toString(url, StandardCharsets.UTF_8));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                result.add(json_var);
             }
-            return json_var;
+            return result;
         }
 
         @Override
-        protected void onPostExecute(JSONObject result) {
-            String ticker = null;
-            try {
-                ticker = result.getString("symbol");
-            } catch (JSONException e) {
-                e.printStackTrace();
+        protected void onPostExecute(ArrayList<JSONObject> results) {
+            for (JSONObject result : results){
+                String ticker = null;
+                try {
+                    ticker = result.getString("symbol");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                DecimalFormat formatter = new DecimalFormat("#,###.00");
+                String logo = "logo_" + ticker;
+                TextView price = (TextView) findViewById(getResources().getIdentifier("textViewPrice_" + ticker , "id", getPackageName()));
+                TextView high = (TextView) findViewById(getResources().getIdentifier("textViewHighPrice_" + ticker , "id", getPackageName()));
+                TextView low = (TextView) findViewById(getResources().getIdentifier("textViewLowPrice_" + ticker , "id", getPackageName()));
+                try {
+                    price.setText("$" + formatter.format(result.getJSONObject("market_data").getJSONObject("current_price").getDouble("usd")));
+                    if (result.getJSONObject("market_data").getInt("price_change_24h")>0)
+                        price.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.increase,0);
+                    else
+                        price.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.decrease,0);
+                    high.setText("$" + formatter.format(result.getJSONObject("market_data").getJSONObject("high_24h").getDouble("usd")));
+                    low.setText("$" + formatter.format(result.getJSONObject("market_data").getJSONObject("low_24h").getDouble("usd")));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-            DecimalFormat formatter = new DecimalFormat("#,###.00");
-            String logo = "logo_" + ticker;
-            TextView price = (TextView) findViewById(getResources().getIdentifier("textViewPrice_" + ticker , "id", getPackageName()));
-            TextView high = (TextView) findViewById(getResources().getIdentifier("textViewHighPrice_" + ticker , "id", getPackageName()));
-            TextView low = (TextView) findViewById(getResources().getIdentifier("textViewLowPrice_" + ticker , "id", getPackageName()));
-            try {
-                price.setText("$" + formatter.format(result.getJSONObject("market_data").getJSONObject("current_price").getDouble("usd")));
-                if (result.getJSONObject("market_data").getInt("price_change_24h")>0)
-                    price.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.increase,0);
-                else
-                    price.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.decrease,0);
-                high.setText("$" + formatter.format(result.getJSONObject("market_data").getJSONObject("high_24h").getDouble("usd")));
-                low.setText("$" + formatter.format(result.getJSONObject("market_data").getJSONObject("low_24h").getDouble("usd")));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            pullToRefresh.setRefreshing(false);
         }
     }
 }
